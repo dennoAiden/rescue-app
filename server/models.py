4f23ca3eec31642f4c773122b8d3be3b4fe944f2
Kush <kuriaisac@gmail.com>
2024-11-06 10:02:04 +0300

    updates changes

---
+++ server/models.py
@@ -0,0 +1,55 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import Enum, func, DateTime
+
+db=SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String, nullable=False)
+    email=db.Column(db.String, unique=True, nullable=False)
+    password=db.Column(db.String, nullable=False)
+    role=db.Column(Enum('admin', 'user'), default='user')
+    created_at=db.Column(db.DateTime, default=func.now())
+
+class Admin(db.Model, SerializerMixin):
+    __tablename__='admins_acts'
+
+    id=db.Column(db.Integer, primary_key=True)
+    incident_report_id=db.Column(db.Integer, db.ForeignKey('incident_reports.id'))
+    action = db.Column(Enum('status_change', 'flagged', 'resolved'))
+    admin_id=db.Column(db.Integer, db.ForeignKey('users.id'), )
+    create_at=db.Column(db.DateTime, default=func.now())
+
+class Report(db.Model, SerializerMixin):
+    __tablename__='incident_reports'
+
+    id=db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title=db.Column(db.String, nullable=False)
+    description=db.Column(db.String, nullable=False)
+    status=db.Column(Enum('under investigation', 'resolved', 'rejected'), default='under investigation')
+    latitude=db.Column(db.Float, nullable=False)
+    longitude=db.Column(db.Float, nullable=False)
+    created_at=db.Column(db.DateTime, default=func.now())
+    updated_at=db.Column(db.DateTime, default=func.now())
+
+class Media(db.Model, SerializerMixin):
+    __tablename__='incident_medias'
+    
+    id=db.Column(db.Integer, primary_key=True)
+    incident_report_id=db.Column(db.Integer, db.ForeignKey('incident_reports.id'), nullable=False)
+    media_type=db.Column(Enum('image', 'video'), nullable=False)
+    media_url=db.Column(db.String, nullable=False)
+
+class Notification(db.Model, SerializerMixin):
+    __tablename__='notifications'
+
+    id=db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    message=db.Column(db.String, nullable=False)
+    read=db.Column(db.Boolean)
+    created_at=db.Column(db.DateTime, default=func.now())
