a27bd81f166a985dc4942d6d2de948cf21165aca
Kush <kuriaisac@gmail.com>
2024-11-26 17:08:03 +0300

    updates changes

--- server/app.py
+++ server/app.py
@@ -54,7 +54,7 @@ api=Api(app)
 
 # Flask-Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'noreplyrescueapp@gmail.com'
 app.config['MAIL_PASSWORD'] = 'qzambnfrbjqpqlwp'
